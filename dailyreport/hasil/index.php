5f1eafcc224b8dc79226196a778e3b0388861c5f
Novando <rizkynovando@gmail.com>
2021-03-29 01:03:03 +0700

    Add files via upload

--- dailyreport/hasil/index.php
+++ dailyreport/hasil/index.php
@@ -14,31 +14,31 @@
 	<title>Laporan</title>
 </head>
 <body>
-	<p>Date : <?= $_GET['tanggal'] ?></p>
-	<p>&#x23F0 Time : 20.30 WIB</p>
-	<br>
-	<p>&#x1F477 PIC MTI : BP. SULTONI</p>
-	<p>&#x1F477 PIC MTI : </p>
-	<p>&#x1F477 PIC BDK : BP. JOKO</p>
-	<p>&#x1F477 PIC BDK : BP. YANA</p>
-	<br>
+	<p>Date : <?= $_GET['tanggal'] ?><br>
+	&#x23F0 Time : 20.30 WIB</p>
+
+	<p>&#x1F477 PIC MTI : BP. SULTONI<br>
+	&#x1F477 PIC MTI : <br>
+	&#x1F477 PIC BDK : BP. JOKO<br>
+	&#x1F477 PIC BDK : BP. YANA</p>
+
 	<p>==================</p>
+
 	<p>&#x1F3ED U DITCH</p>
-	<br>
+
 	<p>&#x1F6A7 MATERIAL  : </p>
-	<p>1. PO I :</p>
-	<p>- U Ditch type 1 : 9.886 set</p>
-	<p>- U Ditch type 2 : 241 set</p>
-	<p>HH</p>
-	<p>- HH Type 1 : 63 Set</p>
-	<br>
-	<p>- HH Type 2 : 5 Set</p>
-	<br>
-	<p>- HH Type 3 : 57 Set</p>
-	<p>==================
-	<br>
+	<p>1. PO I :<br>
+	- U Ditch type 1 : 9.886 set<br>
+	- U Ditch type 2 : 241 set<br>
+	HH<br>
+	- HH Type 1 : 63 Set<br>
+	- HH Type 2 : 5 Set<br>
+	- HH Type 3 : 57 Set</p>
+
+	<p>==================</p>
+
 	<p>&#x1F4A1 UPDATE PROGRESS PRODUKSI :</p>
-	<br>
+
 	<?php
 		include 'sistem.php';
 
@@ -49,26 +49,23 @@
 			while ($row = $result1->fetch_assoc()) {
 				$counter = $counter + 1;
 	?>
-		<p>&#x1F307 Batch <?= $counter; ?> : <?= $row['kode']; ?></p>
-		<p>&#x23F0 Waktu Produksi : <?= $row['jam'] ?> WIB</p>
-		<br>
-		<p>- U Ditch type 1 :</p>
-		<p>&nbsp;U : <?= $po_u1; ?> | <?= $row['u']; ?> | <?php $total_u = $total_u + $row['u']; echo $total_u; ?> | <?= $po_u1 - $total_u; ?></p>
-		<p>&nbsp;T : <?= $po_u1; ?> | <?= $row['t']; ?> | <?php $total_t = $total_t + $row['t']; echo $total_t; ?> | <?= $po_u1 - $total_t; ?></p>
-		<p>&nbsp;C : <?= $po_u1; ?> | <?= $row['c']; ?> | <?php $total_c = $total_c + $row['c']; echo $total_c; ?> | <?= $po_u1 - $total_c; ?></p>
-		<br>
+		<p>&#x1F307 Batch <?= $counter; ?> : <?= $row['kode']; ?><br>
+		&#x23F0 Waktu Produksi : <?= $row['jam'] ?> WIB</p>
+
+		<p>- U Ditch type 1 :<br>
+		&nbsp;U : <?= $po_u1; ?> | <?= $row['u']; ?> | <?php $total_u = $total_u + $row['u']; echo $total_u; ?> | <?= $po_u1 - $total_u; ?><br>
+		&nbsp;T : <?= $po_u1; ?> | <?= $row['t']; ?> | <?php $total_t = $total_t + $row['t']; echo $total_t; ?> | <?= $po_u1 - $total_t; ?><br>
+		&nbsp;C : <?= $po_u1; ?> | <?= $row['c']; ?> | <?php $total_c = $total_c + $row['c']; echo $total_c; ?> | <?= $po_u1 - $total_c; ?></p>
+
 		<p>- U Ditch type  2 : <?= $po_u2; ?> | <?= $row['c2']; ?> | <?php $total_c2 = $total_c2 + $row['c2']; echo $total_c2; ?> | <?= $po_u2 - $total_c2; ?></p>
-		<br>
-		<p>HHx-<?= date_format($date, 'dmy')?>-BDK</p>
-		<br>
-		<p>Waktu Produksi : <?= $row['jam'] ?>  WIB</p>
-		<br>
-		<p>- HH Type 1 : <?= $po_hh1 ; ?> | <?= $row['hh1']; ?> | <?php $total_hh1 = $total_hh1 + $row['hh1']; echo $total_hh1; ?> | <?= $po_hh1 - $total_hh1; ?></p>
-		<br>
-		<p>- HH Type 2 : <?= $po_hh2 ; ?> | <?= $row['hh2']; ?> | <?php $total_hh2 = $total_hh2 + $row['hh2']; echo $total_hh2; ?> | <?= $po_hh2 - $total_hh2; ?></p>
-		<br>
-		<p>- HH Type 3 : <?= $po_hh3 ; ?> | <?= $row['hh3']; ?> | <?php $total_hh3 = $total_hh3 + $row['hh3']; echo $total_hh3; ?> | <?= $po_hh3 - $total_hh3; ?></p>
-		<br>
+		
+		<p>HHx-<?= date_format($date, 'dmy')?>-BDK<br>
+		Waktu Produksi : <?= $row['jam'] ?>  WIB</p>
+		
+		<p>- HH Type 1 : <?= $po_hh1 ; ?> | <?= $row['hh1']; ?> | <?php $total_hh1 = $total_hh1 + $row['hh1']; echo $total_hh1; ?> | <?= $po_hh1 - $total_hh1; ?><br>
+		- HH Type 2 : <?= $po_hh2 ; ?> | <?= $row['hh2']; ?> | <?php $total_hh2 = $total_hh2 + $row['hh2']; echo $total_hh2; ?> | <?= $po_hh2 - $total_hh2; ?><br>
+		- HH Type 3 : <?= $po_hh3 ; ?> | <?= $row['hh3']; ?> | <?php $total_hh3 = $total_hh3 + $row['hh3']; echo $total_hh3; ?> | <?= $po_hh3 - $total_hh3; ?></p>
+		<br>	
 	<?php
 			}
 		}
@@ -80,14 +77,13 @@
 				$counter = $counter + 1;
 	?>
 	
-		<p>&#x1F307 Batch <?= $counter; ?> : <?= $row['kode']; ?></p>
-		<p>&#x23F0 Waktu Produksi : <?= $row['jam'] ?> WIB</p>
-		<br>
-		<p>- U Ditch type 1 :</p>
-		<p>&nbsp;U : <?= $po_u1; ?> | <?= $row['u']; ?> | <?php $total_u = $total_u + $row['u']; echo $total_u; ?> | <?= $po_u1 - $total_u; ?></p>
-		<p>&nbsp;T : <?= $po_u1; ?> | <?= $row['t']; ?> | <?php $total_t = $total_t + $row['t']; echo $total_t; ?> | <?= $po_u1 - $total_t; ?></p>
-		<p>&nbsp;C : <?= $po_u1; ?> | <?= $row['c']; ?> | <?php $total_c = $total_c + $row['c']; echo $total_c; ?> | <?= $po_u1 - $total_c; ?></p>
-		<br>
+		<p>&#x1F307 Batch <?= $counter; ?> : <?= $row['kode']; ?><br>
+		&#x23F0 Waktu Produksi : <?= $row['jam'] ?> WIB</p>
+
+		<p>- U Ditch type 1 :<br>
+		&nbsp;U : <?= $po_u1; ?> | <?= $row['u']; ?> | <?php $total_u = $total_u + $row['u']; echo $total_u; ?> | <?= $po_u1 - $total_u; ?><br>
+		&nbsp;T : <?= $po_u1; ?> | <?= $row['t']; ?> | <?php $total_t = $total_t + $row['t']; echo $total_t; ?> | <?= $po_u1 - $total_t; ?><br>
+		&nbsp;C : <?= $po_u1; ?> | <?= $row['c']; ?> | <?php $total_c = $total_c + $row['c']; echo $total_c; ?> | <?= $po_u1 - $total_c; ?></p>
 	<?php
 			}
 		}
@@ -96,19 +92,19 @@
 		}
 	?>
 	<p>==================</p>
-	<br>
+	
 	<p>&#x1F4DD NEXT PLAN :</p>
-	<p>&nbsp;U ditch : Repair Moulding</p>
-	<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pembesian</p>
-	<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pengecoran uditch.</p>
-	<p>&nbsp;HH : Bongkar begesting</p>
-	<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pembesian </p>
-	<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pengecoran </p>
-	<br>
+	&nbsp;U ditch : Repair Moulding<br>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pembesian<br>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pengecoran uditch.<br>
+	&nbsp;HH : Bongkar begesting<br>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pembesian<br>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pengecoran</p>
+	
 	<p>&#x1F4E2 INFO:</p>
+	
 	<br>
-	<br>
-	<p>&#x1F198 ISSUE:- Repair Moulding</p>
-	<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- penambahan waktu produksi</p>
+	<p>&#x1F198 ISSUE:- Repair Moulding<br>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- penambahan waktu produksi</p>
 </body>
 </html>
